8cf6a4f833859f3a62e93247820242a5ab1fedfd
Duwesh Kumar <dkduwesh07771@gmail.com>
2022-05-28 22:33:04 +0530

    default contxt created

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container, FormControl, Navbar, NavDropdown } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: "80px" }}>
+      <Container>
+        <Navbar.Brand>MASAI CART</Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 500 }}
+            placeholder="Search a product..."
+            className="m-auto"
+          />
+        </Navbar.Text>
+        <NavDropdown title="Cart" id="nav-dropdown">
+          <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.3">
+            Something else here
+          </NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
